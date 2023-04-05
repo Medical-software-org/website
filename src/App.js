a842fafb24bf91145809d6e52f3bf4d6e9e7bcd7
Vuyo Ncume <vuyo.ncume@vcontractor.co.za>
2023-04-05 20:46:40 +0200

    feature: remove home link from the right navbar menu items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import './components/contentBox.css';
-import NavBar from './components/navigationBar';
-import Footer from './components/footer';
-import Home from './components/Home';
-import Privacy from './components/legal/privacy';
-import Faq from './components/legal/freqAsked'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./components/contentBox.css";
+import NavBar from "./components/navigationBar";
+import Footer from "./components/footer";
+import Home from "./components/Home";
+import Privacy from "./components/legal/privacy";
+import Faq from "./components/legal/freqAsked";
 import { useLocation } from "react-router-dom";
 
 // firebase
 import firebase from "firebase/app";
-import 'firebase/firestore';
-import 'firebase/database';
+import "firebase/firestore";
+import "firebase/database";
 
-// TODO: termsOfUse, navbar close icon when expanded on small devices, possible background image on sign up, possible cauresel
+// TODO: re-design send us message and sign up, modal for success, firebase rules fix (chatGPT),
+// fix github deploy issue, rewrite text for 'Our goal' section.
 
 function App() {
-
   const routePath = useLocation();
   const endRef = useRef(null);
   const signUpRef = useRef(null);
@@ -25,30 +25,30 @@ function App() {
 
   const scrollToFooter = (e) => {
     e.preventDefault();
-    endRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const scrollToSignUp = (e) => {
     e.preventDefault();
-    signUpRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    signUpRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const toTop = () => {
     window.scrollTo(0, 0);
-  }
+  };
 
   useEffect(() => {
     toTop();
 
-    // Your web app's Firebase configuration
-    const { 
-      REACT_APP_apiKey, 
-      REACT_APP_authDomain, 
-      REACT_APP_databaseURL, 
-      REACT_APP_projectId, 
-      REACT_APP_storageBucket, 
-      REACT_APP_messagingSenderId, 
-      REACT_APP_appId 
+    // Firebase configuration
+    const {
+      REACT_APP_apiKey,
+      REACT_APP_authDomain,
+      REACT_APP_databaseURL,
+      REACT_APP_projectId,
+      REACT_APP_storageBucket,
+      REACT_APP_messagingSenderId,
+      REACT_APP_appId,
     } = process.env;
 
     try {
@@ -59,9 +59,9 @@ function App() {
         projectId: REACT_APP_projectId,
         storageBucket: REACT_APP_storageBucket,
         messagingSenderId: REACT_APP_messagingSenderId,
-        appId: REACT_APP_appId
+        appId: REACT_APP_appId,
       };
-  
+
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
       setDb(firebase.database());
@@ -69,28 +69,25 @@ function App() {
       // do nothing
       // console.log(error);
     }
-  }, [routePath])
+  }, [routePath]);
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path='/' element={
-          <Home 
-            db={db} 
-            signUpRef={signUpRef} 
-          />
-        }/>              
-        <Route path='/privacy' element={<Privacy />}/>
-        <Route path='/FAQ' element={
-          <Faq 
-            signUpRef={signUpRef} 
-            scrollToSignUp={scrollToSignUp} 
-            scrollToEnd={scrollToFooter} 
-            db={db} 
-          />
-        }/>
-        {/* <Route path='/terms-of-use' element={<Terms />}/> */}
+        <Route path="/" element={<Home db={db} signUpRef={signUpRef} />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route
+          path="/FAQ"
+          element={
+            <Faq
+              signUpRef={signUpRef}
+              scrollToSignUp={scrollToSignUp}
+              scrollToEnd={scrollToFooter}
+              db={db}
+            />
+          }
+        />
       </Routes>
       <Footer reference={endRef} />
     </div>
--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import './navigationBar.css';
-import { Navbar, Nav } from 'react-bootstrap';
-import logo from '../images/logo.svg';
+import React from "react";
+import "./navigationBar.css";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../images/logo.svg";
 import { NavLink } from "react-router-dom";
 
 function navigationBar() {
-    return (
-      <Navbar sticky="top" collapseOnSelect expand="lg" className='shadow p-3 mb-5 bg-white rounded' >
-        <NavLink className='navbar-brand' to="/">
-          <img
-            src={logo}
-            width="55"
-            height="55"
-            className="d-inline-block align-top"
-            alt="Archive Logo"
-          />
-        </NavLink>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" >
-            <NavLink className={(navData) => navData.isActive ? "nav-link active" : "nav-link"} to="/">Home</NavLink>
-            <NavLink className={(navData) => navData.isActive ? "nav-link active" : "nav-link"} to="/privacy">Privacy Policy</NavLink>
-            <NavLink className={(navData) => navData.isActive ? "nav-link active" : "nav-link"} to="/FAQ">FAQ</NavLink>
-            {/* <NavLink className={(navData) => navData.isActive ? "nav-link active" : "nav-link"} to="/terms-of-use">Terms Of Use</NavLink> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
+  return (
+    <Navbar
+      sticky="top"
+      collapseOnSelect
+      expand="lg"
+      className="shadow p-3 mb-5 bg-white rounded"
+    >
+      <NavLink className="navbar-brand" to="/">
+        <img
+          src={logo}
+          width="55"
+          height="55"
+          className="d-inline-block align-top"
+          alt="Archive Logo"
+        />
+      </NavLink>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ms-auto">
+          <NavLink
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+            to="/privacy"
+          >
+            Privacy Policy
+          </NavLink>
+          <NavLink
+            className={(navData) =>
+              navData.isActive ? "nav-link active" : "nav-link"
+            }
+            to="/FAQ"
+          >
+            FAQ
+          </NavLink>
+          {/* <NavLink className={(navData) => navData.isActive ? "nav-link active" : "nav-link"} to="/terms-of-use">Terms Of Use</NavLink> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default navigationBar
+export default navigationBar;
