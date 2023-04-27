224552b6cb8016bfb0959e7968e3584b00bd4e7e
Vuyo Ncume <vuyo.ncume@vcontractor.co.za>
2023-04-27 16:15:27 +0200

    feature: secrets and nexted p tags fix

--- src/components/boxesContentBox.js
+++ src/components/boxesContentBox.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import {globalColors} from './colors';
+import React from "react";
+import { globalColors } from "./colors";
 import { Link } from "react-router-dom";
 
-function boxesContentBox({heading, subHeading}) {
+function boxesContentBox({ heading, subHeading }) {
   return (
-    <div className='center-container-row' style={{color: globalColors.blue}} >
-        <div className='flex-container' >
-            <div className='flex-item-left-border' >
-                <Link style={{textDecoration: "none", color: "inherit"}} to="/privacy">
-                    <div className='box' style={{height: 306}} >
-                        <h1>Privacy Policy</h1>
-                        <i id='icon' class="fa fa-info-circle" ></i>
-                    </div>
-                </Link>
+    <div className="center-container-row" style={{ color: globalColors.blue }}>
+      <div className="flex-container">
+        <div className="flex-item-left-border">
+          <Link
+            style={{ textDecoration: "none", color: "inherit" }}
+            to="/privacy"
+          >
+            <div className="box" style={{ height: 306 }}>
+              <h1>Privacy Policy</h1>
+              <i id="icon" className="fa fa-info-circle"></i>
             </div>
-            <div className='flex-item-right-border' >
-                <Link style={{textDecoration: "none", color: "inherit"}} to="/FAQ">
-                    <div className='box' >
-                        <h1>Frequently Asked Questions</h1>
-                        <i id='icon' class="fa fa-question-circle" ></i>
-                    </div>
-                </Link>
-            </div>
+          </Link>
         </div>
+        <div className="flex-item-right-border">
+          <Link style={{ textDecoration: "none", color: "inherit" }} to="/FAQ">
+            <div className="box">
+              <h1>Frequently Asked Questions</h1>
+              <i id="icon" className="fa fa-question-circle"></i>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/contentBox.js
+++ src/components/contentBox.js
@@ -1,51 +1,59 @@
-import React from 'react';
+import React from "react";
 
-function contentBox({color, header, text1, text2, image, reverse}) {
-
-    if(reverse){
-        return (
-            <div className="flex-container-reverse" style={{background: color, paddingTop: 50, paddingBottom: 50, paddingLeft: 20, paddingRight: 20}} >
-                <div className="flex-item-left">
-                    <img
-                        src={image}
-                        className="image shadow"
-                        alt=""
-                    />
-                </div>
-                <div className="flex-item-right"  >
-                    <p className='pp shadow p-3 mb-5 bg rounded' >
-                        <p style={{fontSize: 36, fontWeight: 'medium'}} >{header}</p>
-                        {text1}
-                        <br/>
-                        <br/>
-                        {text2}
-                    </p>
-                </div>
-            </div>
-        );
-    }
-
+function contentBox({ color, header, text1, text2, image, reverse }) {
+  if (reverse) {
     return (
-        <div className="flex-container" style={{background: color, paddingTop: 50, paddingBottom: 50, paddingLeft: 20, paddingRight: 20}} >
-            <div className="flex-item-left">
-                <img
-                    src={image}
-                    className="image shadow"
-                    alt=""
-                />
-            </div>
-            <div className="flex-item-right" >
-                <p className='pp shadow p-3 mb-5 bg-white rounded' >
-                    <p style={{fontSize: 36, fontWeight: 'medium'}} >{header}</p>
-                    {text1}
-                    <br/>
-                    <br/>
-                    {text2}
-                </p>
-            </div>
+      <div
+        className="flex-container-reverse"
+        style={{
+          background: color,
+          paddingTop: 50,
+          paddingBottom: 50,
+          paddingLeft: 20,
+          paddingRight: 20,
+        }}
+      >
+        <div className="flex-item-left">
+          <img src={image} className="image shadow" alt="" />
         </div>
+        <div className="flex-item-right">
+          <div className="pp shadow p-3 mb-5 bg rounded">
+            <p style={{ fontSize: 36, fontWeight: "medium" }}>{header}</p>
+            {text1}
+            <br />
+            <br />
+            {text2}
+          </div>
+        </div>
+      </div>
     );
+  }
+
+  return (
+    <div
+      className="flex-container"
+      style={{
+        background: color,
+        paddingTop: 50,
+        paddingBottom: 50,
+        paddingLeft: 20,
+        paddingRight: 20,
+      }}
+    >
+      <div className="flex-item-left">
+        <img src={image} className="image shadow" alt="" />
+      </div>
+      <div className="flex-item-right">
+        <div className="pp shadow p-3 mb-5 bg-white rounded">
+          <p style={{ fontSize: 36, fontWeight: "medium" }}>{header}</p>
+          {text1}
+          <br />
+          <br />
+          {text2}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default contentBox
+export default contentBox;
